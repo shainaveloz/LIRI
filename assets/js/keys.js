5a52be35b967cadf23a0f51a11157b5ec3d7b68c
shainaveloz <shaina.veloz@gmail.com>
2016-06-11 07:42:56 -0400

    twitter keys working

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,18 +1,20 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
+var twitterKeys = {
   consumer_key: 'gnZoG3NmUaCsZcJMIhuFjSPYf',
   consumer_secret: 'aPilntTyHLxvCzRNZzqLzyUTe1CLwvXbcNmZlQO7np1A4TsXLK',
   access_token_key: '1362970628-11AFNPC3OjyzoGqlOie6Raju5oPG9gDW4Lcrp0w',
   access_token_secret: 'RaNadSwIwpj3tGDNKOiDQVFMeqovsnzvU8MlNHf3oOKSz',
 }
 
+module.exports = twitterKeys;
+
 
 // NPM Package for reading and writing files
 var fs = require('fs'); 
 
 // This block of code will create a file called "movies.txt".
-// It will then print "Inception, Die Hard" in the file
+// It will then print "I Want It That Way"
 fs.writeFile("random.txt", 'spotify-this-song, "I Want It That Way"', function(err) {
     
 	// If the code experiences any errors it will log the error to the console. 
@@ -20,6 +22,6 @@ fs.writeFile("random.txt", 'spotify-this-song, "I Want It That Way"', function(e
         return console.log(err);
     }
 
-    // Otherwise, it will print: "movies.txt was updated!"
+    // Otherwise, it will print: "random.txt was updated!"
     console.log("random.txt was updated!");
 }); 
--- assets/js/liri.js
+++ assets/js/liri.js
@@ -1,1 +1,30 @@
-var keys = require('keys.js');
+var keys = require('./keys.js');
+var twitter = require('twitter');
+var tweets = new twitter(keys.twitterKeys);
+
+var params = {screen_name: 'shay2420'};
+tweets.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
+
+var search = process.argv[2];
+
+
+// request npm package 
+var request = require('request');
+
+// a request to the OMDB API with the movie specified
+request('http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&r=json', function (error, response, body) {
+
+	// If the request is successful (i.e. if the response status code is 200)
+	if (!error && response.statusCode == 200) {
+
+		// Parse the body of the site and recover just the imdbRating
+		// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
+		console.log("The movie's is: " + JSON.parse(body)["Title"])
+	}
+});
